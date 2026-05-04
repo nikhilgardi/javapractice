9b7c2a22a873ed8a822d7ae83cf522d9d23edbcd
nikhilgardi@gmail.com <nikhilgardi@gmail.com>
2026-05-04 23:24:15 +0530

    added thyleaf doc

---
+++ src/main/java/com/java/java8/practice/Java8.java
@@ -0,0 +1,12 @@
+package com.java.java8.practice;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.java.java8.Employee;
+
+public class Java8 {
+	
+	List<Employee> employees=Arrays.asList(new Employee(1,"Mamta",100000,"Doctor"));
+
+}
